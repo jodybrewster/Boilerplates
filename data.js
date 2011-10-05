538874ff083499202e576f0b686810fb8ca182fd
Shaun Dunne <thedarkpoet@gmail.com>
2011-10-05 20:52:09 +0100

    Initial Commit

---
+++ data.js
@@ -0,0 +1,65 @@
+var boilerplates = [
+  {
+    name: "JS Library Boilerplate",
+    description: "Start building a custom Javascript library today",
+    url: "http://benalman.com/projects/javascript-library-boilerplate/",
+    source: "https://github.com/cowboy/javascript-library-boilerplate/blob/master/qsa.js",
+  },
+  {
+    name: "HTML 5 Boilerplate",
+    description: "A great starting point for those HTML5 projects",
+    url: "http://html5boilerplate.com/",
+    source: "http://github.com/paulirish/html5-boilerplate/zipball/v2.0stripped",
+  },
+  {
+    name: "jQuery Boilerplate",
+    description: "Start build jQuery plugins with this boilerplate",
+    url: "http://jqueryboilerplate.com/",
+    source: "https://github.com/downloads/zenorocha/jquery-boilerplate/jquery-boilerplate-v2.01.zip",
+  },
+  {
+    name: "Skeleton Boilerplate",
+    description: "A Beautiful Boilerplate for Responsive, Mobile-Friendly Development",
+    url: "http://www.getskeleton.com/",
+    source: "https://github.com/dhgamache/Skeleton/zipball/master",
+  },
+  {
+    name: "Gridless Boilerplate",
+    description: "Optionated HTML5 and CSS3 boilerplate for making mobile first responsive, cross-browser websites",
+    url: "http://thatcoolguy.github.com/gridless-boilerplate/",
+    source: "https://github.com/thatcoolguy/gridless-boilerplate/tarball/master",
+  },
+  {
+    name: "CSS Boilerplate V3",
+    description: "A simple CSS framework to get you started",
+    url: "http://code.google.com/p/css-boilerplate/",
+    source: "http://css-boilerplate.googlecode.com/files/css-boilerplate-v03.zip",
+  },
+  {
+    name: "Chrome Boilerplate",
+    description: "A boilerplate for Chrome extension and app development",
+    url: "http://softwareas.com/chrome-boilerplate",
+    source: "https://github.com/mahemoff/chrome-boilerplate/zipball/master",
+  },
+  {
+    name: "iOS Boilerplate",
+    description: "A code base for iOS projects",
+    url: "http://iosboilerplate.com/index.html",
+    source: "https://github.com/gimenete/iOS-boilerplate/zipball/master",
+  },
+  {
+    name: "HTML5 Mobile Boilerplate",
+    description: "It's for your mobile and in HTML5",
+    url: "http://html5boilerplate.com/mobile",
+    source: "https://github.com/shichuan/mobile-html5-boilerplate/zipball/v2.0",
+  },
+  {
+    name: "Boilerplate Boilerplate",
+    description: "Build ur own Boilerplate",
+    url: "https://github.com/ajpiano/boilerplate-boilerplate",
+    source: "https://github.com/ajpiano/boilerplate-boilerplate/zipball/master",
+  }
+  
+  
+
+];
